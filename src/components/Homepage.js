a739f93f20bfbd2db0fbdc55f641c0fcab264d6e
hackhim18 <triphimanshu18@gmail.com>
2021-11-06 19:17:49 +0530

    c6-06/11

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,6 +7,7 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 //import Cryptocurrencies from './Cryptocurrencies';
 //import News from './News';
 //import Loader from './Loader';
+import { Cryptocurrencies,News } from '../components';
 
 const { Title } = Typography;
 
@@ -27,7 +28,17 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
         <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
          <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
-      </Row>
+         </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
       
       
     </>
